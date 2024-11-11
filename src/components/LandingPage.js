3b73db8096713293c19a01dd18fe27bc713ae1f8
ryouol <royluo05@gmail.com>
2024-11-10 16:36:45 -0800

    Button change

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -11,12 +11,12 @@ const LandingPage = () => {
             <img src={logo} alt="Limitless Logo" className="logo" />
             <h1 className="title">We’re Building Limitless, Democratizing Venture Capital.</h1>
             <div className="buttons">
-                <Link to="/learn-more">
-                    <button className="button-89" role="button">Learn More</button>
-                </Link>
-                <Link to="/contact-us">
-                    <button className="button-89" role="button">Contact Us</button>
-                </Link>
+            <Link to="/learn-more">
+                <button className="button-89">Learn More</button>
+            </Link>
+            <Link to="/contact-us">
+                <button className="button-89">Contact Us</button>
+            </Link>
             </div>
         </div>
     );
